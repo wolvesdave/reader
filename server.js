8d3a6cb732af2201448d830e087f48ad68672429
David Koppe <wolvesdave@gmail.com>
2016-11-04 16:13:09 -0400

    :

--- server.js
+++ server.js
@@ -9,15 +9,20 @@ var routes = require('./app/routes');
 var app = express();
 var morgan = require('morgan');
 
+/* export STORMPATH_CLIENT_APIKEY_ID=1GTP266RYID4QBWVN9HL07VD6
+export STORMPATH_CLIENT_APIKEY_SECRET=sNzyD5+GmyWkH4UMJSyzASYVZ7QLqoOLb3Vmj1W5YdE
+export STORMPATH_APPLICATION_HREF=https://api.stormpath.com/v1/applications/3kMK5hMEcrWa2h09OXY8EX */
+
 console.log('about to connect to Stormpath with ./config/stormpath_apikey.properties');
 
 app.use(morgan);
+
 app.use(stormpath.init(app, {
      apiKeyFile: './config/stormpath_apikey.properties',
      application: 'https://api.stormpath.com/v1/applications/3kMK5hMEcrWa2h09OXY8EX',
-/*     secretKey: security.stormpath_secret_key */
-     secretKey: '3kMK5hMEcrWa2h09OXY8EX'
+     secretKey: security.stormpath_secret_key
  }));
+
 var port = 8000;
 
 console.log('about to connect to ' + db.url);
